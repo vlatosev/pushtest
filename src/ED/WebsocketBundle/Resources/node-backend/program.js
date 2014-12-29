be0295c60d7b056c52e241dec5026233de75a465
vladimir.tosev <vladimir.tosev@etondigital.com>
2014-12-29 16:57:10 +0100

    Set up socket io front

--- src/ED/WebsocketBundle/Resources/node-backend/program.js
+++ src/ED/WebsocketBundle/Resources/node-backend/program.js
@@ -1,19 +1,19 @@
 var http = require('http'),
     express = require('express'),
-    app = express()
+    app = express(),
+    server = app.listen(3017, function(){}),
+    io = require('socket.io')(server)
     ;
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-var server = app.listen(3017, function(){}),
-    io = require('socket.io')(server)
-    ;
 
 io.on('connection', function(socket){
     console.log('listeni cng on *:3000');
     socket.on('messaga', function(msg){
+        msg.message = socket.id;
         io.emit('messaga', msg);
         console.log('sent ' + msg.message);
     });
