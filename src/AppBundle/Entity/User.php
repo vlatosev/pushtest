7835fbedae77c48fb9d49f7dac860fb4fc1e28cd
vladimir.tosev <vladimir.tosev@etondigital.com>
2015-01-06 16:45:03 +0100

    Create guzzle request

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\UserRepository")
+ */
+class User extends \FOS\UserBundle\Model\User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+}
--- src/ED/WebsocketBundle/Controller/DefaultController.php
+++ src/ED/WebsocketBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace ED\WebsocketBundle\Controller;
 
+use Guzzle\Http\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -14,8 +15,11 @@ class DefaultController extends Controller
      */
     public function indexAction($name)
     {
+        $client = new Client('http://localhost:3017');
+        $response = $client->get('/')->send();
         return array(
-            'name' => $name
+            'name' => $name,
+            'response' => $response->getBody()
         );
     }
 }
